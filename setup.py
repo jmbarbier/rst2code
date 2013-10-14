be198dab94c344d4ce0123e0b1352fc6dfb300b2
Jean-Matthieu BARBIER <jm.barbier@solidev.net>
2013-10-14 03:18:02 +0200

    Added authors and setup.py
    Signed-off-by: Jean-Matthieu BARBIER <jm.barbier@solidev.net>

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = "0.0.1"
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Documentation',
+    'Topic :: Software Development',
+    'Topic :: Text Processing :: Markup'
+]
+
+INSTALL_REQUIRES = [
+    "Pygments>=1.6",
+    "Sphinx>=1.1.3",
+    "docutils>=0.11"
+]
+
+setup(
+    name = "rst2code",
+    description = "reStructuredText literate programming tool",
+    classifiers = CLASSIFIERS,
+    install_requires = INSTALL_REQUIRES,
+    version = VERSION,
+    author = "Jean-Matthieu BARBIER",
+    author_email = "jm.barbier@solidev.net",
+    url="https://github.com/jmbarbier/rst2code",
+    download_url="https://github.com/jmbarbier/rst2code/archive/v"+VERSION+".tar.gz",
+    py_modules = "rst2code"
+)
